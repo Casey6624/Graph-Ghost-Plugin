e2b836e18bf3bfa3f4d61b329eb8f8a81169ff4d
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-15 19:15:41 +0100

    Playing around with a first commit

---
+++ src/bg/background.js
@@ -0,0 +1,9 @@
+const btnAPIMe = document.getElementById("btnAPIMe");
+btnAPIMe.addEventListener("click", watchForElements)
+
+function watchForElements() {
+  console.log("APIMe Initiated")
+  console.log(document)
+  document.addEventListener("mouseenter", function (e) {
+  })
+}
