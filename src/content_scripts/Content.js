65e21f8521d1b826771702c125a1ff36bbc4fd74
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-20 18:46:40 +0100

    Added a counter which automatically updates and keeps track of how many elements are currently selected

--- src/content_scripts/Content.js
+++ src/content_scripts/Content.js
@@ -19,18 +19,36 @@ document.getElementById("g-g-d-Cancel").addEventListener("click", function () {
     console.log("Cancelling API Creation")
 })
 
+/* Counter Actions */
+const counterLabel = document.createElement("div")
+counterLabel.setAttribute("id", "g-g-d-Counter")
+counterLabel.innerHTML = `${elements.length} Elements Selected`
+document.body.appendChild(counterLabel);
+document.getElementById("g-g-d-Counter").addEventListener("click", function () {
+    console.log("Cancelling API Creation")
+})
+
+function updateCounter() {
+    counterLabel.innerHTML = `${elements.length} Elements Selected`
+}
+
 // Code to handle the selection
 document.addEventListener("click", function (e) {
-    if (e.target.id === "g-g-d-Submit" || e.target.id === "g-g-d-Cancel" || e.target.nodeName === "HTML" || e.target.nodeName === "BODY") return
+    // Validation
+    if (e.target.id === "g-g-d-Submit" || e.target.id === "g-g-d-Cancel" || e.target.id === "g-g-d-Counter" || e.target.nodeName === "HTML" || e.target.nodeName === "BODY") return
+    // Check if item already exists in array, remove it if so
     if (elements.includes(e.target)) {
         elements = elements.filter(el => el !== e.target)
+        updateCounter()
         e.target.style.border = null
         e.target.style.padding = null
         console.log(elements)
         return
     }
+    // Add the new item as it is not in the array
     elements.push(e.target)
     console.log(elements)
+    updateCounter()
     e.target.style.border = "2px double red"
     e.target.style.padding = "0.5rem"
 })
