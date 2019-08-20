c6a488a2c1260f598d91f7b8b7360901fe9a438a
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-20 18:26:28 +0100

    Added Create & Cancel buttons which have an absolute position and visible when the plugin is active. I'm going to next remove the browser action as we don't really need a seperate pop up to trigger the plugin

--- src/content_scripts/Content.js
+++ src/content_scripts/Content.js
@@ -1,10 +1,32 @@
 let elements = []
+let selecting = false
 
+/* Submit Actions */
+const submitButton = document.createElement("div")
+submitButton.setAttribute("id", "g-g-d-Submit")
+submitButton.innerHTML = "Create GraphQL API"
+document.body.appendChild(submitButton);
+document.getElementById("g-g-d-Submit").addEventListener("click", function () {
+    console.log("Submitting to Server")
+})
+
+/* Cancel Actions */
+const cancelButton = document.createElement("div")
+cancelButton.setAttribute("id", "g-g-d-Cancel")
+cancelButton.innerHTML = "Cancel"
+document.body.appendChild(cancelButton);
+document.getElementById("g-g-d-Cancel").addEventListener("click", function () {
+    console.log("Cancelling API Creation")
+})
+
+// Code to handle the selection
 document.addEventListener("click", function (e) {
+    if (e.target.id === "g-g-d-Submit" || e.target.nodeName === "HTML" || e.target.nodeName === "BODY") return
     if (elements.includes(e.target)) {
         elements = elements.filter(el => el !== e.target)
         e.target.style.border = null
         e.target.style.padding = null
+        console.log(elements)
         return
     }
     elements.push(e.target)
