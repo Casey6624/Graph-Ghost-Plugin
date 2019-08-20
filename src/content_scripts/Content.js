081e17658d0b852d32f0e5d3af051d2dd832544c
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-20 17:11:05 +0100

    Now managed to get adding/removing elements working within content.js. We can manage which pieces of data we would like to grab to create the API. Next I am going to add HTML buttons and overlays to confirm/cancel the selection

--- src/content_scripts/Content.js
+++ src/content_scripts/Content.js
@@ -1,6 +1,12 @@
-const elements = []
+let elements = []
 
 document.addEventListener("click", function (e) {
+    if (elements.includes(e.target)) {
+        elements = elements.filter(el => el !== e.target)
+        e.target.style.border = null
+        e.target.style.padding = null
+        return
+    }
     elements.push(e.target)
     console.log(elements)
     e.target.style.border = "2px double red"
