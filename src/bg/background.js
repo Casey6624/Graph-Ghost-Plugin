4fbbe661527ed683833178518497482fa7be4431
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-27 12:48:16 +0100

    Added localStorage permissions which allows us to keep track of app state and will be used to toggle between item selecting and ordinary browsing

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(null, { file: "startSelection.js" });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  //chrome.tabs.executeScript(null, { file: "./startSelection.js" });
+  chrome.storage.sync.get("g-g-dState", function(items) {
+    console.log("old val " + items["g-g-dState"]);
+    let newValue = items["g-g-dState"] === "true" ? "false" : "true";
+
+    chrome.storage.sync.set({ "g-g-dState": `${newValue}` }, function() {
+      console.log("new val " + newValue);
+    });
+  });
 });
--- src/bg/startSelection.js
+++ src/bg/startSelection.js
@@ -1,54 +1,65 @@
-let elements = []
-let selecting = false
+let elements = [];
+let selecting = false;
 
 // Submit Actions
-const submitButton = document.createElement("div")
-submitButton.setAttribute("id", "g-g-d-Submit")
-submitButton.innerHTML = "Create GraphQL API"
+const submitButton = document.createElement("div");
+submitButton.setAttribute("id", "g-g-d-Submit");
+submitButton.innerHTML = "Create GraphQL API";
 document.body.appendChild(submitButton);
-document.getElementById("g-g-d-Submit").addEventListener("click", function () {
-    console.log("Submitting to Server")
-})
+document.getElementById("g-g-d-Submit").addEventListener("click", function() {
+  if (elements.length === 0) {
+    window.alert("You haven't selected any data to make an API from!");
+    return;
+  }
+  console.log("Submitting to Server");
+});
 
 // Cancel Actions
-const cancelButton = document.createElement("div")
-cancelButton.setAttribute("id", "g-g-d-Cancel")
-cancelButton.innerHTML = "Cancel"
+const cancelButton = document.createElement("div");
+cancelButton.setAttribute("id", "g-g-d-Cancel");
+cancelButton.innerHTML = "Cancel";
 document.body.appendChild(cancelButton);
-document.getElementById("g-g-d-Cancel").addEventListener("click", function () {
-    console.log("Cancelling API Creation")
-})
+document.getElementById("g-g-d-Cancel").addEventListener("click", function() {
+  console.log("Cancelling API Creation");
+});
 
 // Counter Actions
-const counterLabel = document.createElement("div")
-counterLabel.setAttribute("id", "g-g-d-Counter")
-counterLabel.innerHTML = `${elements.length} Elements Selected`
+const counterLabel = document.createElement("div");
+counterLabel.setAttribute("id", "g-g-d-Counter");
+counterLabel.innerHTML = `${elements.length} Elements Selected`;
 document.body.appendChild(counterLabel);
-document.getElementById("g-g-d-Counter").addEventListener("click", function () {
-    console.log("Cancelling API Creation")
-})
+document.getElementById("g-g-d-Counter").addEventListener("click", function() {
+  console.log("Cancelling API Creation");
+});
 
 function updateCounter() {
-    counterLabel.innerHTML = `${elements.length} Elements Selected`
+  counterLabel.innerHTML = `${elements.length} Elements Selected`;
 }
 
 // Code to handle the selection
-document.addEventListener("click", function (e) {
-    // Validation
-    if (e.target.id === "g-g-d-Submit" || e.target.id === "g-g-d-Cancel" || e.target.id === "g-g-d-Counter" || e.target.nodeName === "HTML" || e.target.nodeName === "BODY") return
-    // Check if item already exists in array, remove it if so
-    if (elements.includes(e.target)) {
-        elements = elements.filter(el => el !== e.target)
-        updateCounter()
-        e.target.style.border = null
-        e.target.style.padding = null
-        console.log(elements)
-        return
-    }
-    // Add the new item as it is not in the array
-    elements.push(e.target)
-    console.log(elements)
-    updateCounter()
-    e.target.style.border = "2px double red"
-    e.target.style.padding = "0.5rem"
-})
+document.addEventListener("click", function(e) {
+  // Validation
+  if (
+    e.target.id === "g-g-d-Submit" ||
+    e.target.id === "g-g-d-Cancel" ||
+    e.target.id === "g-g-d-Counter" ||
+    e.target.nodeName === "HTML" ||
+    e.target.nodeName === "BODY"
+  )
+    return;
+  // Check if item already exists in array, remove it if so
+  if (elements.includes(e.target)) {
+    elements = elements.filter(el => el !== e.target);
+    updateCounter();
+    e.target.style.border = null;
+    e.target.style.padding = null;
+    console.log(elements);
+    return;
+  }
+  // Add the new item as it is not in the array
+  elements.push(e.target);
+  console.log(elements);
+  updateCounter();
+  e.target.style.border = "2px double red";
+  e.target.style.padding = "0.5rem";
+});
