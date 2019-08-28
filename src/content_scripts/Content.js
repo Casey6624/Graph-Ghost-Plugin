58fcef15d6cd5a32fa00e6b144155e286360b472
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-08-28 19:12:44 +0100

    Pure JavaScript is proving a little tricky to create a preformant builder. We could be where React would preform stronger. I'm going to research to see if we can embed CRA inside of a plugin

--- src/content_scripts/Content.js
+++ src/content_scripts/Content.js
@@ -1,54 +1,76 @@
-/* let elements = []
-let selecting = false
+let elements = [];
+let state = false;
 
-// Submit Actions
-const submitButton = document.createElement("div")
-submitButton.setAttribute("id", "g-g-d-Submit")
-submitButton.innerHTML = "Create GraphQL API"
-document.body.appendChild(submitButton);
-document.getElementById("g-g-d-Submit").addEventListener("click", function () {
-    console.log("Submitting to Server")
-})
+console.log("yayayayay");
 
-// Cancel Actions
-const cancelButton = document.createElement("div")
-cancelButton.setAttribute("id", "g-g-d-Cancel")
-cancelButton.innerHTML = "Cancel"
-document.body.appendChild(cancelButton);
-document.getElementById("g-g-d-Cancel").addEventListener("click", function () {
-    console.log("Cancelling API Creation")
-})
+function checkIfEditing() {
+  chrome.storage.sync.get("g-g-dState", function(items) {
+    if (items["g-g-dState"] === "true") state = true;
+  });
+}
 
-// Counter Actions
-const counterLabel = document.createElement("div")
-counterLabel.setAttribute("id", "g-g-d-Counter")
-counterLabel.innerHTML = `${elements.length} Elements Selected`
-document.body.appendChild(counterLabel);
-document.getElementById("g-g-d-Counter").addEventListener("click", function () {
-    console.log("Cancelling API Creation")
-})
+if (state) {
+  // Submit Actions
+  const submitButton = document.createElement("div");
+  submitButton.setAttribute("id", "g-g-d-Submit");
+  submitButton.innerHTML = "Create GraphQL API";
+  document.body.appendChild(submitButton);
+  document.getElementById("g-g-d-Submit").addEventListener("click", function() {
+    console.log("Submitting to Server");
+  });
 
-function updateCounter() {
-    counterLabel.innerHTML = `${elements.length} Elements Selected`
+  // Cancel Actions
+  const cancelButton = document.createElement("div");
+  cancelButton.setAttribute("id", "g-g-d-Cancel");
+  cancelButton.innerHTML = "Cancel";
+  document.body.appendChild(cancelButton);
+  document.getElementById("g-g-d-Cancel").addEventListener("click", function() {
+    // Here we want to set localStorage to "false" as we want to stop using the editor mode
+    chrome.storage.sync.set({ "g-g-dState": "false" }, function() {
+      console.log("Graph Ghost is closing...");
+    });
+  });
+
+  // Counter Actions
+  const counterLabel = document.createElement("div");
+  counterLabel.setAttribute("id", "g-g-d-Counter");
+  counterLabel.innerHTML = `${elements.length} Elements Selected`;
+  document.body.appendChild(counterLabel);
+  document
+    .getElementById("g-g-d-Counter")
+    .addEventListener("click", function() {
+      console.log("Cancelling API Creation");
+    });
+
+  function updateCounter() {
+    counterLabel.innerHTML = `${elements.length} Elements Selected`;
+  }
 }
 
 // Code to handle the selection
-document.addEventListener("click", function (e) {
-    // Validation
-    if (e.target.id === "g-g-d-Submit" || e.target.id === "g-g-d-Cancel" || e.target.id === "g-g-d-Counter" || e.target.nodeName === "HTML" || e.target.nodeName === "BODY") return
-    // Check if item already exists in array, remove it if so
-    if (elements.includes(e.target)) {
-        elements = elements.filter(el => el !== e.target)
-        updateCounter()
-        e.target.style.border = null
-        e.target.style.padding = null
-        console.log(elements)
-        return
-    }
-    // Add the new item as it is not in the array
-    elements.push(e.target)
-    console.log(elements)
-    updateCounter()
-    e.target.style.border = "2px double red"
-    e.target.style.padding = "0.5rem"
-}) */
+document.addEventListener("click", function(e) {
+  // Validation
+  if (
+    e.target.id === "g-g-d-Submit" ||
+    e.target.id === "g-g-d-Cancel" ||
+    e.target.id === "g-g-d-Counter" ||
+    e.target.nodeName === "HTML" ||
+    e.target.nodeName === "BODY"
+  )
+    return;
+  // Check if item already exists in array, remove it if so
+  if (elements.includes(e.target)) {
+    elements = elements.filter(el => el !== e.target);
+    updateCounter();
+    e.target.style.border = null;
+    e.target.style.padding = null;
+    console.log(elements);
+    return;
+  }
+  // Add the new item as it is not in the array
+  elements.push(e.target);
+  console.log(elements);
+  updateCounter();
+  e.target.style.border = "2px double red";
+  e.target.style.padding = "0.5rem";
+});
