ef5ab3c4c6b5fb8d62e66cd5d00e8f17130319bb
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2020-01-09 12:43:49 +0000

    I have added validation to the plugin to check whether or not an attribute title has been added. validateAttrName achieves this by either returning true or false

--- src/content_scripts/Content.js
+++ src/content_scripts/Content.js
@@ -6,118 +6,135 @@ let btnSubmit, btnCancel, btnAddAttri, counterLabel, txtAttri;
 // this will be eventually be https://graphghost.co.uk/api or something similar
 const NODE_SERVER = "http://127.0.0.1:4500/crawlme";
 
-chrome.extension.sendMessage({}, function(response) {
+/* chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
+      clearInterval(readyStateCheckInterval); */
 
-      // Add a listener to listen to the message from background.js
-      chrome.runtime.onMessage.addListener(gotMessage);
+// Add a listener to listen to the message from background.js
+chrome.runtime.onMessage.addListener(gotMessage);
 
-      function gotMessage(message, sender, sendResponse) {
-        if (message === "true") {
-          // set up submit button if not defined
-          if (btnSubmit === undefined) {
-            document.addEventListener("click", e => selectingEntities(e));
-            btnSubmit = document.createElement("div");
-            btnSubmit.setAttribute("id", "g-g-d-Submit");
-            btnSubmit.innerHTML = "Done";
-            document.body.appendChild(btnSubmit);
-            btnSubmit.addEventListener("click", function() {
-              postToServer();
-            });
-          }
-          // set up cancel button if not defined
-          if (btnCancel === undefined) {
-            // Cancel Actions
-            btnCancel = document.createElement("div");
-            btnCancel.setAttribute("id", "g-g-d-Cancel");
-            btnCancel.innerHTML = "Cancel";
-            document.body.appendChild(btnCancel);
-            btnCancel.addEventListener("click", function() {
-              // Here we want to set localStorage to "false" as we want to stop using the editor mode
-              chrome.storage.sync.set({ "g-g-dState": "false" }, function() {
-                console.log("Graph Ghost is closing...");
-                clearentitiesAndStyling();
-                removeButtonsAndListeners();
-                document.removeEventListener("click", e =>
-                  selectingEntities(e)
-                );
-              });
-            });
-          }
-          // set up cancel button if not defined
-          // TODO: GGET THIS WORKING START FROM HERE
-          if (txtAttri === undefined) {
-            // Cancel Actions
-            txtAttri = document.createElement("input");
-            txtAttri.setAttribute("id", "g-g-d-txtAttri");
-            txtAttri.type = "text";
-            txtAttri.placeholder = "Attribute Name";
-            document.body.appendChild(txtAttri);
-            txtAttri.addEventListener("click", function() {
-              // Here we want to set localStorage to "false" as we want to stop using the editor mode
-              chrome.storage.sync.set({ "g-g-dState": "false" }, function() {
-                console.log("Graph Ghost is closing...");
-                clearentitiesAndStyling();
-                removeButtonsAndListeners();
-                document.removeEventListener("click", e =>
-                  selectingEntities(e)
-                );
-              });
-            });
-          }
-          // set up cancel button if not defined
-          if (btnAddAttri === undefined) {
-            // Cancel Actions
-            btnAddAttri = document.createElement("div");
-            btnAddAttri.setAttribute("id", "g-g-d-addAttri");
-            btnAddAttri.innerHTML = "Add";
-            document.body.appendChild(btnAddAttri);
-            btnAddAttri.addEventListener("click", function() {});
-          }
-          // set up counter if not defined
-          if (counterLabel === undefined) {
-            // Counter Actions
-            counterLabel = document.createElement("div");
-            counterLabel.setAttribute("id", "g-g-d-Counter");
-            counterLabel.innerHTML = `${entities.length} entities Selected`;
-            document.body.appendChild(counterLabel);
-            counterLabel.addEventListener("click", function() {
-              console.log("Cancelling API Creation");
-            });
-          }
-        } else {
-          removeButtonsAndListeners();
-          document.removeEventListener("click", e => selectingEntities(e));
-        }
-      }
-
-      function removeButtonsAndListeners() {
-        console.log("removing event listener!");
+function gotMessage(message, sender, sendResponse) {
+  // set up submit button if not defined
+  if (btnSubmit === undefined) {
+    document.addEventListener("click", e => selectingEntities(e));
+    btnSubmit = document.createElement("div");
+    btnSubmit.setAttribute("id", "g-g-d-Submit");
+    btnSubmit.innerHTML = "Done";
+    document.body.appendChild(btnSubmit);
+    btnSubmit.addEventListener("click", function() {
+      postToServer();
+    });
+  }
+  // set up cancel button if not defined
+  if (btnCancel === undefined) {
+    // Cancel Actions
+    btnCancel = document.createElement("div");
+    btnCancel.setAttribute("id", "g-g-d-Cancel");
+    btnCancel.innerHTML = "Cancel";
+    document.body.appendChild(btnCancel);
+    btnCancel.addEventListener("click", function() {
+      // Here we want to set localStorage to "false" as we want to stop using the editor mode
+      chrome.storage.sync.set({ "g-g-dState": "false" }, function() {
+        console.log("Graph Ghost is closing...");
+        clearEntitiesAndStyling();
+        removeButtonsAndListeners();
         document.removeEventListener("click", e => selectingEntities(e));
-        btnSubmit.removeEventListener("click");
-        btnCancel.removeEventListener("click");
-        btnSubmit.parentNode.removeChild(btnSubmit);
-        btnSubmit = undefined;
-        btnCancel.parentNode.removeChild(btnCancel);
-        btnCancel = undefined;
-        counterLabel.parentNode.removeChild(counterLabel);
-        counterLabel = undefined;
-      }
+      });
+    });
+  }
+  // set up cancel button if not defined
+  if (txtAttri === undefined) {
+    // Cancel Actions
+    txtAttri = document.createElement("input");
+    txtAttri.setAttribute("id", "g-g-d-txtAttri");
+    txtAttri.type = "text";
+    txtAttri.placeholder = "Attribute Name";
+    document.body.appendChild(txtAttri);
+  }
+  // set up cancel button if not defined
+  if (btnAddAttri === undefined) {
+    // Cancel Actions
+    btnAddAttri = document.createElement("div");
+    btnAddAttri.setAttribute("id", "g-g-d-addAttri");
+    btnAddAttri.innerHTML = "Add";
+    document.body.appendChild(btnAddAttri);
+    btnAddAttri.addEventListener("click", function() {});
+  }
+  // set up counter if not defined
+  if (counterLabel === undefined) {
+    // Counter Actions
+    counterLabel = document.createElement("div");
+    counterLabel.setAttribute("id", "g-g-d-Counter");
+    counterLabel.innerHTML = `${entities.length} entities Selected`;
+    document.body.appendChild(counterLabel);
+    counterLabel.addEventListener("click", function() {
+      console.log("Cancelling API Creation");
+    });
+  }
+}
 
-      function clearentitiesAndStyling() {
-        entities.forEach(function(el, index) {
-          // remove styling and clear entities array
-          el.style.border = "none";
-          el.style.padding = "0rem";
-        });
-        entities = [];
-      }
+function removeButtonsAndListeners() {
+  console.log("removing event listener!");
+  document.removeEventListener("click", e => selectingEntities(e));
+  if (btnSubmit !== undefined) {
+    btnSubmit.removeEventListener("click");
+  }
+  if (btnSubmit !== undefined) {
+    btnCancel.removeEventListener("click");
+  }
+  if (btnSubmit.parentNode !== undefined) {
+    btnSubmit.parentNode.removeChild(btnSubmit);
+    btnSubmit = undefined;
+  }
+  if (btnCancel.parentNode !== undefined) {
+    btnCancel.parentNode.removeChild(btnCancel);
+    btnCancel = undefined;
+  }
+  if (counterLabel.parentNode !== undefined) {
+    counterLabel.parentNode.removeChild(counterLabel);
+    counterLabel = undefined;
+  }
+}
 
-      function postToServer() {
-        chrome.runtime.sendMessage({ entities: entities });
-        /*         const data = {
+function validateAttrName() {
+  if (txtAttri.value.trim() === "") {
+    return false;
+  }
+  const conditions = [
+    "!",
+    "?",
+    " ",
+    "*",
+    "/",
+    "\\",
+    "`",
+    "#",
+    "~",
+    "=",
+    "+",
+    "-"
+  ];
+  const testInput = conditions.some(cond => txtAttri.value.includes(cond));
+  // if false then contains illegal characters
+  return !testInput;
+}
+
+function clearEntitiesAndStyling() {
+  entities.forEach(function({ style }, index) {
+    // remove styling and clear entities array
+    style.border = "none";
+    style.padding = "0rem";
+  });
+  entities = [];
+}
+
+function postToServer() {
+  const validAttrName = validateAttrName();
+  console.log(validAttrName);
+  return;
+  chrome.runtime.sendMessage({ entities: entities });
+  /*         const data = {
           entities: entities,
           // This needs changing to be dynamic via the google tabs API
           url: "http://localhost/ggd/index.html"
@@ -134,41 +151,40 @@ chrome.extension.sendMessage({}, function(response) {
           .catch(err => {
             return err;
           }); */
-      }
+}
 
-      console.log("Content Script is running");
+console.log("Content Script is running");
 
-      function updateCounter() {
-        if (counterLabel === undefined) return;
-        counterLabel.innerHTML = `${entities.length} entities Selected`;
-      }
+function updateCounter() {
+  if (counterLabel === undefined) return;
+  counterLabel.innerHTML = `${entities.length} entities Selected`;
+}
 
-      const selectingEntities = function({ target }) {
-        if (
-          target.id === "g-g-d-Submit" ||
-          target.id === "g-g-d-Cancel" ||
-          target.id === "g-g-d-Counter" ||
-          target.id === "g-g-d-txtAttri" ||
-          target.nodeName === "HTML" ||
-          target.nodeName === "BODY"
-        )
-          return;
-        // Check if item already exists in array, remove it if so
-        if (entities.includes(target)) {
-          entities = entities.filter(el => el !== target);
-          updateCounter();
-          target.style.border = null;
-          target.style.padding = null;
-          console.log(entities);
-          return;
-        }
-        // Add the new item as it is not in the array
-        entities.push(target);
-        console.log(entities);
-        updateCounter();
-        target.style.border = "2px double red";
-        target.style.padding = "0.5rem";
-      };
-    }
-  }, 10);
-});
+const selectingEntities = function({ target }) {
+  if (
+    target.id === "g-g-d-Submit" ||
+    target.id === "g-g-d-Cancel" ||
+    target.id === "g-g-d-Counter" ||
+    target.id === "g-g-d-txtAttri" ||
+    target.nodeName === "HTML" ||
+    target.nodeName === "BODY"
+  )
+    return;
+  // Check if item already exists in array, remove it if so
+  if (entities.includes(target)) {
+    entities = entities.filter(el => el !== target);
+    updateCounter();
+    target.style.border = null;
+    target.style.padding = null;
+    console.log(entities);
+    return;
+  }
+  // Add the new item as it is not in the array
+  entities.push(target);
+  console.log(entities);
+  updateCounter();
+  target.style.border = "2px double red";
+  target.style.padding = "0.5rem";
+};
+//}
+/*   }, 10); */
